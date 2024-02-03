95f0a32e519eafd4071febdab510fd0103eaeb71
ChooWon Sir 서주원 <43027225+chws@users.noreply.github.com>
2024-02-03 05:16:49 -0500

    Update summarize.py

--- puru/summarize.py
+++ puru/summarize.py
@@ -1,35 +1,43 @@
-import openai
+from openai import OpenAI
 import json
+import re
 
-# Load your API key from the JSON file
-try:
-    with open('key.json', 'r') as file:
-        api_key_data = json.load(file)
-        api_key = api_key_data.get('api_key', '')
+def summarize():
+    # Load your API key from the JSON file
+    try:
+        with open('/Users/choo/GithubProjects/CMU_Spring_2024/tartanhack_purus/puru/key.json', 'r') as file:
+            api_key_data = json.load(file)
+            api_key = api_key_data.get('api_key', '')
 
-    # Set your OpenAI API key
-    openai.api_key = api_key
+        # Set your OpenAI API key
+        client = OpenAI(api_key=api_key)
+        
+        # Text to be summarized
+        with open('output.txt', 'r', encoding='utf-8') as file:
+            text_to_summarize = file.read()
 
-    # Text to be summarized
-    with open('output.txt', 'r', encoding='utf-8') as file:
-        text_to_summarize = file.read()
+        # Ask OpenAI to summarize the text
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role":"user", "content": text_to_summarize+". give me ingredients in a python list format."}],
+            )
 
-    # Ask OpenAI to summarize the text
-    response = openai.Completion.create(
-        engine="davinci-002",
-        prompt=text_to_summarize+". now summarize the ingredients",
-        max_tokens=100
-    )
+        # Print the summarized text
+        print("Original Text:")
+        print(text_to_summarize)
+        print("\nSummarized Text:")
+        raw = response.choices[0].message.content
+        print("gpt answer")
+        print(raw)
+        pattern = r'"([^"]*)"'
+        matches = re.findall(pattern, raw)
 
-    # Print the summarized text
-    print("Original Text:")
-    print(text_to_summarize)
-    print("\nSummarized Text:")
-    print(response['choices'][0]['text'])
+        print(matches)
+        return matches
 
-except FileNotFoundError:
-    print("Error: 'key.json' file not found. Make sure the file exists in the correct location.")
-except json.JSONDecodeError:
-    print("Error: 'key.json' is not a valid JSON file.")
-except openai.error.InvalidRequestError as e:
-    print(f"OpenAI API request error: {e}")
+    except FileNotFoundError:
+        print("Error: 'key.json' file not found. Make sure the file exists in the correct location.")
+    except json.JSONDecodeError:
+        print("Error: 'key.json' is not a valid JSON file.")
+    # except openai.error.InvalidRequestError as e:
+    #     print(f"OpenAI API request error: {e}")
