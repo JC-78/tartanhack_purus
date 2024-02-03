9d47597595d3f4966f52b6cb29aef3af132fe5ee
ChooWon Sir 서주원 <43027225+chws@users.noreply.github.com>
2024-02-03 05:15:53 -0500

    Create main.py

---
+++ puru/main.py
@@ -0,0 +1,13 @@
+from text_extraction import extract_image_to_text
+from summarize import summarize
+from processing import count_allergen_ingredients, count_harmful_ingredients, check_ingredients
+
+if __name__ == "__main__":
+    img_path = 'puru/white.png'
+    extract_image_to_text(img_path)
+    ingredients = summarize()
+    print(">>>>>>>>>>>>>>ingredients")
+    print(ingredients)
+    count_allergen_ingredients(ingredients)
+    count_harmful_ingredients(ingredients)
+    check_ingredients(ingredients)
